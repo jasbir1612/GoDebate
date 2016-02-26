c2bc4213619a67ebe0c97d9d0c018015794c0727
jasbir1612 <jasbir1612@gmail.com>
2016-02-26 16:00:45 +0530

    list added

--- app/src/main/java/com/gtbit/godebate/MainActivity.java
+++ app/src/main/java/com/gtbit/godebate/MainActivity.java
@@ -1,13 +1,31 @@
 package com.gtbit.godebate;
 
+import android.content.res.Resources;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+
+    Toolbar toolbar;
+    ListView topicsList;
+    ArrayAdapter adapter;
+    String titles[];
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        Resources resources = getResources();
+        titles = resources.getStringArray(R.array.titles);
+
+        topicsList = (ListView) findViewById(R.id.topics_list);
+        adapter = new ArrayAdapter(this, R.layout.topics_row,R.id.topics, titles);
+        topicsList.setAdapter(adapter);
     }
 }
