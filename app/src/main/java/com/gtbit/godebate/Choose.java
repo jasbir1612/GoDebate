b1cbb26b3f907af043b9b1180f2be87972b9c037
jasbir1612 <jasbir1612@gmail.com>
2016-02-29 01:24:23 +0530

    minor changes in create topic

--- app/src/main/java/com/gtbit/godebate/Choose.java
+++ app/src/main/java/com/gtbit/godebate/Choose.java
@@ -5,7 +5,10 @@ import android.content.Intent;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
 
@@ -57,11 +60,36 @@ public class Choose extends AppCompatActivity implements View.OnClickListener {
                         @Override
                         public void onClick(DialogInterface dialog, int which) {
 
+                            String name = topicInput.getText().toString();
+                            Intent i = new Intent(Choose.this, Main2Activity.class);
+                            i.putExtra("Topic", name);
+                            startActivity(i);
+
+
                         }
                     });
 
                     final AlertDialog dialog = builder.show();
                     dialog.getButton(android.app.AlertDialog.BUTTON_POSITIVE).setEnabled(false);
+
+                    topicInput.addTextChangedListener(new TextWatcher() {
+                        @Override
+                        public void beforeTextChanged(CharSequence charSequence, int i, int i2, int i3) {
+                        }
+
+                        @Override
+                        public void onTextChanged(CharSequence charSequence, int i, int i2, int i3) {
+                            if (charSequence.length() > 0 && charSequence.length() <= 22) {
+                                dialog.getButton(android.app.AlertDialog.BUTTON_POSITIVE).setEnabled(true);
+                            } else {
+                                dialog.getButton(android.app.AlertDialog.BUTTON_POSITIVE).setEnabled(false);
+                            }
+                        }
+
+                        @Override
+                        public void afterTextChanged(Editable editable) {
+                        }
+                    });
 
                 }
             }
